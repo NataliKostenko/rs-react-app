1fe6e90e521645d71335019b461fcb0d9b780caa
NataliKostenko <kostenko.nata81@gmail.com>
2025-02-03 12:54:20 +0200

    Update Overlay.tsx

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-
 interface OverlayProps {
   isEnabled: boolean;
 }
 
-class Overlay extends React.Component<OverlayProps> {
-  render(): React.ReactNode {
-    return (
-      <div
-        className="overlay"
-        style={{ display: this.props.isEnabled ? 'block' : 'none' }}
-      ></div>
-    );
-  }
+export default function Overlay(props: OverlayProps) {
+  return (
+    <div
+      className="overlay"
+      style={{ display: props.isEnabled ? 'block' : 'none' }}
+    ></div>
+  );
 }
-export default Overlay;
