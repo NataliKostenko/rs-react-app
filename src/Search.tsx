18a13927fe19a4f66ca21151ad19aadab6532fb4
NataliKostenko <kostenko.nata81@gmail.com>
2025-02-04 20:32:32 +0200

    Update Search.tsx

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,26 +1,18 @@
-import React, { ChangeEventHandler, MouseEventHandler } from 'react';
+import { MouseEventHandler } from 'react';
 import search from '/search.svg';
 import './Search.css';
-
 interface SearchProps {
   searchTerm: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-class Search extends React.Component<SearchProps> {
-  render() {
-    return (
-      <div className="topControls">
-        <input
-          defaultValue={this.props.searchTerm}
-          id="searchTerm"
-          type="text"
-        />
-        <button type="submit" onClick={this.props.onClick}>
-          <img src={search} className="search" alt="search" />
-        </button>
-      </div>
-    );
-  }
+export default function Search(props: SearchProps) {
+  return (
+    <div className="topControls">
+      <input defaultValue={props.searchTerm} id="searchTerm" type="text" />
+      <button type="submit" onClick={props.onClick}>
+        <img src={search} className="search" alt="search" />
+      </button>
+    </div>
+  );
 }
-export default Search;
