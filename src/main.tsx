38949925afcdfea1c699ffcbb8557776cb38db65
NataliKostenko <kostenko.nata81@gmail.com>
2025-02-06 21:08:49 +0200

    routing and 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.tsx';
+import PageNotFound from './PageNotFound.tsx';
 import ErrorBoundary from './ErrorBoundary.tsx';
 
 const rootElemet = document.getElementById('root');
@@ -10,7 +12,12 @@ if (rootElemet) {
   createRoot(rootElemet).render(
     <StrictMode>
       <ErrorBoundary>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<App />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
       </ErrorBoundary>
     </StrictMode>
   );
